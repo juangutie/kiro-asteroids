c50ce64d1b17f98719f08cd09c7d3811d225b502
Juan Gutierrez <24966258+juangutie@users.noreply.github.com>
2026-04-02 21:57:50 -0400

    refactor(service-worker.js): for easier versioning

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,40 @@
-const CACHE = 'kiro-asteroids_v1';
-const ASSETS = ['/kiro-asteroids/', '/kiro-asteroids/index.html', '/kiro-asteroids/favicon.ico'];
+const REPOSITORY = "kiro-asteroids";
+const CACHE_VERSION = "v2";
+const URLS = [
+    "/",
+    "/favicon.ico",
+    "/index.html",
+].map((url) => `/${REPOSITORY}${url}`);
 
-self.addEventListener('install', e =>
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)))
-);
+self.addEventListener("install", (event) => {
+    event.waitUntil(
+        caches.open(`${REPOSITORY}_${CACHE_VERSION}`).then((cache) =>
+            cache.addAll(URLS)
+        )
+    );
+});
 
-self.addEventListener('fetch', e =>
-  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)))
-);
+self.addEventListener("activate", (event) => {
+    event.waitUntil(
+        caches.keys().then((keys) => Promise.all(
+            keys.filter((key) =>
+                key.startsWith(REPOSITORY) && !key.endsWith(CACHE_VERSION)
+            ).map((key) =>
+                caches.delete(key)
+            )  
+        ))
+    )
+});
+
+self.addEventListener("fetch", (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) =>
+            response ?? fetch(event.request).catch((error) => 
+                new Response(error, {
+                    status: 408,
+                    headers: {"Content-Type": "text/plain"}
+                })
+            )
+        )
+    );
+});
