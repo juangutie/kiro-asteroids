deca21e19730dc20835c0aeb4fcffa109fa6ff48
Juan Gutierrez <24966258+juangutie@users.noreply.github.com>
2026-04-06 22:00:31 -0400

    refactor(service-worker.js): separate cache types
    Split cache into static and dynamic. Static cache should only
    update when the service worker updates. Dynamic cache is
    everything else.

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,7 @@
 const REPOSITORY = "kiro-asteroids";
-const CACHE_VERSION = "v10";
+const VERSION = "v11";
+const STATIC_CACHE = `${REPOSITORY}_static_${VERSION}`;
+const DYNAMIC_CACHE = `${REPOSITORY}_dynamic`;
 const URLS = [
     "/",
     "/favicon.ico",
@@ -20,7 +22,7 @@ const URLS = [
 
 self.addEventListener("install", (event) => {
     event.waitUntil(
-        caches.open(`${REPOSITORY}_${CACHE_VERSION}`).then((cache) =>
+        caches.open(STATIC_CACHE).then((cache) =>
             cache.addAll(URLS)
         )
     );
@@ -30,7 +32,9 @@ self.addEventListener("activate", (event) => {
     event.waitUntil(
         caches.keys().then((keys) => Promise.all(
             keys.filter((key) =>
-                key.startsWith(REPOSITORY) && !key.endsWith(CACHE_VERSION)
+                key.startsWith(REPOSITORY)
+                && key != STATIC_CACHE
+                && key != DYNAMIC_CACHE
             ).map((key) =>
                 caches.delete(key)
             )  
@@ -39,26 +43,55 @@ self.addEventListener("activate", (event) => {
 });
 
 self.addEventListener("fetch", (event) => {
-    event.respondWith(
-        caches.match(event.request).then((response) =>
-            response ?? fetch(event.request).then(
-                (response) => {
-                    if (response.ok) {
-                        const responseCopy = response.clone();
-                        event.waitUntil(
-                            caches.open(`${REPOSITORY}_${CACHE_VERSION}`).then((cache) =>
-                                cache.put(event.request, responseCopy)
-                            )
-                        );
-                    }
-                    return response;
-                },
-                (error) => 
-                    new Response(error, {
-                        status: 408,
-                        headers: {"Content-Type": "text/plain"}
-                    })
-            )
-        )
-    );
+    event.respondWith((async () => {
+        // Case 1: return from static cache
+        const staticCache = await caches.open(STATIC_CACHE);
+        const staticResponse = await staticCache.match(event.request);
+
+        if (staticResponse) {
+            return staticResponse;
+        }
+
+        // Case 2: return from network and add to static cache
+        const shouldBeInStaticCache = URLS.includes(event.request);
+
+        if (shouldBeInStaticCache) {
+            const updatedRequest = new Request(event.request, {cache: "no-cache"});
+            const networkResponse = await fetch(updatedRequest).catch((error) =>
+                new Response(error, {
+                    status: 408,
+                    headers: {"Content-Type": "text/plain"}
+                })
+            );
+
+            if (networkResponse.ok) {
+                const responseCopy = networkResponse.clone();
+                event.waitUntil(
+                    staticCache.put(updatedRequest, responseCopy)
+                );
+            }
+
+            return networkResponse;
+        }
+
+        // Case 3: return from network, backed up by dynamic cache
+        const dynamicCache = await caches.open(DYNAMIC_CACHE);
+        const dynamicResponse = await dynamicCache.match(event.request);
+
+        const networkResponse = await fetch(event.request).catch((error) =>
+            dynamicResponse ?? new Response(error, {
+                status: 408,
+                headers: {"Content-Type": "text/plain"}
+            })
+        );
+
+        if ((networkResponse.ok || networkResponse.status === 0) && networkResponse !== dynamicResponse) {
+            const responseCopy = networkResponse.clone();
+            event.waitUntil(
+                dynamicCache.put(event.request, responseCopy)
+            );
+        }
+
+        return networkResponse;
+    })());
 });
